d5cd50981f18ea301781ce73f1b0c37711b7c3e8
chandra reddy <chandrareddy058@gmail.com>
2017-10-21 17:15:34 +0530

    public folder added

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const bodyParser=require('body-parser');
 const morgan=require('morgan');
 const async=require('async');
 const request=require('request');
+const expressHbs=require('express-handlebars');
 
 const app=express();
+app.engine('.hbs',expressHbs({defaultLayout:'layout',extname:'.hbs'}));
+app.set('view engine','hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(morgan('dev'));
 
 app.get('/',(req,res,next)=>{
-    res.json('Fuck your ass');
+    res.render('main/home');
     });
 
 app.listen(3000,(err)=>{
