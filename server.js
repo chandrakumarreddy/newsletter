414981cc28d1bd034d9466be3f02560910b89102
chandra reddy <chandrareddy058@gmail.com>
2017-10-22 08:31:24 +0530

    newsletter template updated

--- server.js
+++ server.js
@@ -13,10 +13,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(morgan('dev'));
 
-app.get('/',(req,res,next)=>{
-    res.render('main/home');
-    });
-
+app.route('/')
+        .get((req,res,next)=>{
+            res.render('main/home');
+              })
+        .post((req,res,next)=>{
+          request({
+       url: 'https://us17.api.mailchimp.com/3.0/lists/4978726d24/members',
+       method: 'POST',
+       headers: {
+         'Authorization': 'randomUser e35bb7cb0cbfd6533569ea82741e4c7e-us17',
+         'Content-Type': 'application/json'
+       },
+       json: {
+         'email_address': req.body.email,
+         'status': 'subscribed'
+       },fun(err,res,body){
+          if(err){
+            console.log('error');
+          }else{
+            console.log('Succeed');
+            res.redirect('/');
+          }
+        }
+      });
+      });
 app.listen(process.env.PORT || 3000,(err)=>{
 if(err){
     console.log('connection error');
